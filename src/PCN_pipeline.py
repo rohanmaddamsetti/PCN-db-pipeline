5c8870ba96388a90fffbf8b654f01e1500d2fabf
Rohan Maddamsetti <rohan.maddamsetti@gmail.com>
2024-07-08 15:23:27 -0400

    fixed bug in polars dataframe join

--- src/PCN_pipeline.py
+++ src/PCN_pipeline.py
@@ -1607,7 +1607,7 @@ def run_PIRA_on_all_genomes(multiread_alignment_dir, themisto_replicon_ref_dir, 
         my_PIRA_PCN_estimate_DataFrame = PIRAGenomeDataFrame.with_columns([PIRA_PCN_estimate_series])
 
         ## now append the DataFrame for this genome to the big DataFrame for all genomes.
-        all_PIRA_estimates_DataFrame.append(my_PIRA_PCN_estimate_DataFrame)
+        all_PIRA_estimates_DataFrame.join(my_PIRA_PCN_estimate_DataFrame, how = 'right')
 
     ## now save all_PIRA_estimates_DataFrame to disk.
     all_PIRA_estimates_DataFrame.to_csv(PIRA_PCN_csv_file)
